2265735ac899f567d4ec93fe3d01350cf6fedd4b
hati-wolf <foxclaw@ymail.ne.jp>
2024-12-24 10:22:18 +0900

    Update main.py

--- discord_qustion_bot/main.py
+++ discord_qustion_bot/main.py
@@ -1,16 +1,7 @@
-import io
-import json
-import csv
 import os
-
-import aiohttp
-from pathlib import Path
-import configparser
-
+import json
 import discord
-from discord import activity
 from discord.ext import commands
-from discord.ext.commands import Bot
 
 
 class UserHelp(commands.DefaultHelpCommand):
@@ -25,29 +16,28 @@ class UserHelp(commands.DefaultHelpCommand):
 
     def get_ending_note(self):
         return (
-            "質問箱Bot\n",
+            "質問箱Bot\n"
             "BotのDMにメッセージや画像を送ると、指定されたチャンネルに匿名化されて送信されます。"
         )
 
 
 def main():
-    with open('./info.json', 'r') as f:
-        json_load = json.load(f)
-    TOKEN = json_load['token']
+    # GitHub Secretsからトークンを取得
+    TOKEN = os.getenv("DISCORD_TOKEN")
+    if not TOKEN:
+        raise ValueError("DISCORD_TOKEN is not set in the environment variables")
 
     prefix = '~'
-    bot = Bot(
+    bot = commands.Bot(
         command_prefix=prefix,
         help_command=UserHelp(),
         activity=discord.Game(name=f"send DM or {prefix}help")
     )
+
+    # cog拡張機能をロード
     bot.load_extension('cog')
     bot.run(TOKEN)
 
 
 if __name__ == "__main__":
     main()
-
-TOKEN = os.getenv("DISCORD_TOKEN")  # GitHub Secretsから取得
-if not TOKEN:
-    raise ValueError("DISCORD_TOKEN is not set in the environment variables")
