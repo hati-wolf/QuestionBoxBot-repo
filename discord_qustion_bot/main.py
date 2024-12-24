cb76e42c35f9ec572c6b40832c33435be43bf9c9
hati-wolf <foxclaw@ymail.ne.jp>
2024-12-24 10:57:44 +0900

    Update main.py

--- discord_qustion_bot/main.py
+++ discord_qustion_bot/main.py
@@ -1,9 +1,7 @@
 import json
-import os
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
-
 
 class UserHelp(commands.DefaultHelpCommand):
     def __init__(self):
@@ -21,32 +19,24 @@ class UserHelp(commands.DefaultHelpCommand):
             "BotのDMにメッセージや画像を送ると、指定されたチャンネルに匿名化されて送信されます。"
         )
 
-
 def main():
-    # GitHub Secrets から Discord Token を取得
-    TOKEN = os.getenv("DISCORD_TOKEN")
-    if not TOKEN:
-        raise ValueError("DISCORD_TOKEN is not set in the environment variables")
+    with open('./info.json', 'r') as f:
+        json_load = json.load(f)
+    TOKEN = json_load['token']
 
-    # GitHub Secrets から Channel ID を取得
-    CHANNEL_ID = os.getenv("DISCORD_CHANNEL_ID")
-    if not CHANNEL_ID:
-        raise ValueError("DISCORD_CHANNEL_ID is not set in the environment variables")
+    # Intentsの設定
+    intents = discord.Intents.default()
+    intents.message_content = True  # メッセージ内容を取得するために必要な権限を設定
 
-    # Bot の設定
     prefix = '~'
     bot = Bot(
         command_prefix=prefix,
         help_command=UserHelp(),
-        activity=discord.Game(name=f"send DM or {prefix}help")
+        activity=discord.Game(name=f"send DM or {prefix}help"),
+        intents=intents  # intentsを渡す
     )
-
-    # Cog (質問ボット) をロード
     bot.load_extension('cog')
-
-    # Bot を起動
     bot.run(TOKEN)
-
 
 if __name__ == "__main__":
     main()
