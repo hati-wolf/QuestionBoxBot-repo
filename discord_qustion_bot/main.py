3395543d663b952fdc4c2fe59926b2e80badeffe
hati-wolf <foxclaw@ymail.ne.jp>
2024-12-24 10:53:59 +0900

    no message

--- discord_qustion_bot/main.py
+++ discord_qustion_bot/main.py
@@ -1,7 +1,8 @@
-import os
 import json
+import os
 import discord
 from discord.ext import commands
+from discord.ext.commands import Bot
 
 
 class UserHelp(commands.DefaultHelpCommand):
@@ -16,26 +17,34 @@ class UserHelp(commands.DefaultHelpCommand):
 
     def get_ending_note(self):
         return (
-            "質問箱Bot\n"
+            "質問箱Bot\n",
             "BotのDMにメッセージや画像を送ると、指定されたチャンネルに匿名化されて送信されます。"
         )
 
 
 def main():
-    # GitHub Secretsからトークンを取得
+    # GitHub Secrets から Discord Token を取得
     TOKEN = os.getenv("DISCORD_TOKEN")
     if not TOKEN:
         raise ValueError("DISCORD_TOKEN is not set in the environment variables")
 
+    # GitHub Secrets から Channel ID を取得
+    CHANNEL_ID = os.getenv("DISCORD_CHANNEL_ID")
+    if not CHANNEL_ID:
+        raise ValueError("DISCORD_CHANNEL_ID is not set in the environment variables")
+
+    # Bot の設定
     prefix = '~'
-    bot = commands.Bot(
+    bot = Bot(
         command_prefix=prefix,
         help_command=UserHelp(),
         activity=discord.Game(name=f"send DM or {prefix}help")
     )
 
-    # cog拡張機能をロード
+    # Cog (質問ボット) をロード
     bot.load_extension('cog')
+
+    # Bot を起動
     bot.run(TOKEN)
 
 
